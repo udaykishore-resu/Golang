db71698c23b068031701afe5e124c8d8f4d89bb6
udaykishore-resu <166478692+udaykishore-resu@users.noreply.github.com>
2024-12-02 01:34:13 -0500

    committing code changes related to timesheet-app-react and timesheet-app

--- Projects/timesheet-app/services/timesheet_service.go
+++ Projects/timesheet-app/services/timesheet_service.go
@@ -1,12 +1,24 @@
 package services
 
 import (
+	"fmt"
 	"timesheet-app/database"
 	"timesheet-app/models"
 )
 
 func SubmitTimesheet(timesheet models.TimesheetDetail) error {
 	db := database.GetDB()
-	_, err := db.Exec("INSERT INTO Timesheets (EmployeeID,ProjectID,SubProjectID, JiraSnowID, TaskDescription, HoursSpent, Comments) VALUES (?,?, ?,?,?,?,?)", timesheet.EmployeeID, timesheet.ProjectID, timesheet.SubProjectID, timesheet.JiraSnowID, timesheet.TaskDescription, timesheet.HoursSpent, timesheet.Comments)
+	query := "INSERT INTO Timesheets (EmployeeID,ProjectID,SubProjectID, JiraSnowID, TaskDescription, HoursSpent, Comments) VALUES (?,?, ?,?,?,?,?)"
+	args := []interface{}{
+		timesheet.ProjectID,
+		timesheet.SubProjectID,
+		timesheet.JiraSnowID,
+		timesheet.TaskDescription,
+		timesheet.HoursSpent,
+		timesheet.Comments,
+	}
+	_, err := db.Exec(query, args...)
+	fmt.Println("query: ", query)
+	fmt.Println("args: ", args)
 	return err
 }
